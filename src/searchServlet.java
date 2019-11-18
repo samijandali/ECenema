ade566a940b854a04bb740f9d4e2d0c22f898c95
samijandali <samirifai18@gmail.com>
2019-11-17 23:51:05 -0500

    Movie Update with new columns

--- src/searchServlet.java
+++ src/searchServlet.java
@@ -33,7 +33,7 @@ public class searchServlet extends HttpServlet {
             HttpSession session = request.getSession();
             ArrayList<Movie> movies = new ArrayList<Movie>();
             while(rs.next()){
-                movies.add(new Movie(rs.getInt(6), rs.getString(1),rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5)));
+                movies.add(new Movie(rs.getInt(6), rs.getString(1),rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13) );
                 session.setAttribute("movieList", movies);
             }
         } catch (Exception e) {
